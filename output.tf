72b7889aee5b44edc8a4d13d513032f3a950261e
Sony Huynh <hpsony94@gmail.com>
2020-11-20 22:37:02 +0700

    Revert "disabled output"
    This reverts commit 25efe5722b5c7b2dc61a393a72cd0f07240865fc.

--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
-# output "user_minio_user" {
-#   value = "${minio_iam_user.bucket.*.id}"
-# }
-# 
-# output "minio_user_status" {
-#   value = "${minio_iam_user.bucket.*.status}"
-# }
-# 
-# output "minio_user_secret" {
-#   value = "${minio_iam_user.bucket.*.secret}"
-# }
+output "user_minio_user" {
+  value = "${minio_iam_user.bucket.*.id}"
+}
+
+output "minio_user_status" {
+  value = "${minio_iam_user.bucket.*.status}"
+}
+
+output "minio_user_secret" {
+  value = "${minio_iam_user.bucket.*.secret}"
+}
